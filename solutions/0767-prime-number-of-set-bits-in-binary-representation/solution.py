84255ee70fbcc08390149454e870f141b4660b3a
Nikhil Rachawar <78250252+nikhil149@users.noreply.github.com>
2026-02-21 15:00:20 +0000

    Sync LeetCode submission Runtime - 47 ms (81.12%), Memory - 19.4 MB (40.49%)

---
+++ solutions/0767-prime-number-of-set-bits-in-binary-representation/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        primes = {2, 3, 5, 7, 11, 13, 17, 19}
+        ans = 0
+        for i in range(left, right + 1):
+            set_bits = bin(i).count('1')
+            if set_bits in primes:
+                ans += 1
+        return ans
